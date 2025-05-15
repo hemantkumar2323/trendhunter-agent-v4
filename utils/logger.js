ce1cdea7b25ac0df2bcd9fbc2f92d8b305b40507
hemantkumar2323 <freelanceb23@gmail.com>
2025-05-15 16:17:09 +0530

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,23 @@
-// Placeholder for /utils/logger.js
+// utils/logger.js
+
+const fs = require('fs');
+const path = require('path');
+
+const logDir = path.join(__dirname, '../data/logs');
+if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
+
+function logToFile(filename, message) {
+  const logPath = path.join(logDir, filename);
+  const logMessage = `[${new Date().toISOString()}] ${message}\n`;
+  fs.appendFileSync(logPath, logMessage, 'utf8');
+}
+
+function logJSON(filename, data) {
+  const filePath = path.join(logDir, filename);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+module.exports = {
+  logToFile,
+  logJSON
+};
